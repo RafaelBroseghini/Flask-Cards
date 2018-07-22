3ad8026dc71734f6762a3726fd4fce2aaa829830
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-21 20:08:47 -0500

    fixed element parenting

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -8,12 +8,20 @@ $('.ui.dropdown')
 
 function showModal(el) {
   $('.ui.long.modal', function(){
-    let card = $(el).parent().parent(),
+    let card = $(el).parent().parent().parent(),
     topic = card.find(".topic").text(),
     question = card.find(".question").text(),
     modaltitle = document.querySelector(".modaltitle")
 
+    console.log(card);
+    console.log(topic);
+    console.log(question);
+    // console.log();
+    // console.log();
+    
+    
     modaltitle.textContent = topic;
+    console.log(modaltitle.textContent);
 
     $(".highlight").remove();
     $(".modalcontent").append(question)
